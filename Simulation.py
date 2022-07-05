d7d87c1beb279d876ad28c03c6f6f3c0d1a300d4
carendv <J.C.deVries-1@student.tudelft.nl>
2022-07-05 16:20:02 +0200

    Add statistics
    Add statistics about random walk

--- Simulation.py
+++ Simulation.py
@@ -141,5 +141,7 @@ def simulation(NAMM = 1, NLOB = 1, shocks=0, days=3, seed=100):
 
     return outputs
 
-results = simulation(NAMM=1, NLOB=1, shocks=1, days=15, seed=100)
+results = simulation(NAMM=1, NLOB=1, shocks=0, days=5, seed=100)
+results[0].randomWalk()
+results[1].randomWalk()
 
