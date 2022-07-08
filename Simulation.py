e65511b628d8faaa78d8dc1055db2f8fc882bcc1
carendv <J.C.deVries-1@student.tudelft.nl>
2022-07-08 12:19:59 +0200

    Update plots

--- Simulation.py
+++ Simulation.py
@@ -85,16 +85,30 @@ def visualizeResults(results):
     ####################
     ### Create plots ###
     ####################
-    plotWithInformed(statistics.times, statistics.prices, points)
-    plotWithInformed(statistics.times, statistics.spread, points)
+    plotWithInformed(statistics, points)
+    # plotWithInformed(statistics.times, statistics.prices, points, "Price")
+    # plotWithInformed(statistics.times, statistics.spread, points, "Unit spread")
+    # plotWithInformed(statistics.times, statistics.sellVol, points, "Cumulative sell volume")
+    # plotWithInformed(statistics.times, statistics.buyVol, points, "Cumulative buy volume")
 
-def plotWithInformed(times, data, points):
+def plotWithInformed(data, points):
+    names = ["Price", "Unit spread", "Cumulative sell volume", "cumulative buy volume"]
+    dataS = [data.prices, data.spread, data.sellVol, data.buyVol]
+    num = len(names)
+    rows = 2
+    columns = num/rows
+    
     plt.figure()
-    plt.plot(times, data)    
-    for i in range(round(len(points)/3)):
-        plt.axvspan(times[points[i*3]], times[points[i*3+1]], alpha = 0.5, color='g')
-        plt.axvspan(times[points[i*3+1]], times[points[i*3+2]], alpha = 0.5, color='r')
-    plt.show()
+    for i in range(num):
+        plt.subplot(rows, int(np.ceil(columns)), i+1)
+        plt.plot(data.times, dataS[i])
+        plt.xlabel("Time (s)")
+        plt.ylabel(names[i])
+        plt.grid(True)    
+        for i in range(round(len(points)/3)):
+            plt.axvspan(data.times[points[i*3]], data.times[points[i*3+1]], alpha = 0.5, color='g')
+            plt.axvspan(data.times[points[i*3+1]], data.times[points[i*3+2]], alpha = 0.5, color='r')
+    plt.tight_layout()  
     
 def simulation(NAMM = 1, NLOB = 1, shocks=0, days=3, seed=100): 
     outputs = []
